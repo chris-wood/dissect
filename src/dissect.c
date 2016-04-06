3b55bdb6e8a721a5d49c411acc05ab94e2d87b70
Christopher Wood <christopherwood07@gmail.com>
2016-04-06 06:01:21 -0700

    Pull in JSON

--- src/dissect.c
+++ src/dissect.c
@@ -4,9 +4,15 @@
 
 #include <stdio.h>
 
+#include "packet.h"
+
+// usage:
+// - read from stdin and dump to stdout
+// - specify filters (as SQL query or a list of fields to extract)
+// - specify different output formats per packet (CSV, JSON, etc)
+
 int
 main(int argc, char **argv)
 {
-    // TODO
     printf("dissect v0.1\n");
 }
